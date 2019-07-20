d1f2e01796eacef4802e79fa7f7bf11707dc15af
Tal Rosenberger <tal.rosenberger@gmail.com>
2019-07-20 08:09:04 +0300

    CountryId

---
+++ country-general/src/com/MyCommon/Country.java
@@ -0,0 +1,37 @@
+package com.MyCommon;
+
+// Maybe I can move Country class / project to infrastructure-geography
+public class Country {
+    private String name;
+    //private InternationalPrefix internationalPrefix; // 972
+
+    public Country(String name) {
+        this.setName(name);
+    }
+
+    public Country(){
+        this.setName("");
+    }
+
+    /*
+    public Country (Country other, InternationalPrefix internationalPrefix){
+        this.setName(other.getName());
+        this.internationalPrefix = internationalPrefix;
+    }
+    */
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
