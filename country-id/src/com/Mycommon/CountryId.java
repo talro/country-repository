18adea6f4a9dc6a540e74a8a947bb79c353b7309
Tal Rosenberger <tal.rosenberger@gmail.com>
2019-09-21 18:52:07 +0300

    CountryId     constants added

--- country-id/src/com/Mycommon/CountryId.java
+++ country-id/src/com/Mycommon/CountryId.java
@@ -2,8 +2,10 @@ package com.Mycommon;
 
 public class CountryId {
 
-    public static final long USA = 1;
-    public static final long ISRAEL = 2;
+    public static final long USA_CODE = 1;
+    public static final long ISRAEL_CODE = 2;
+    public static final CountryId USA = new CountryId(USA_CODE);
+    public static final CountryId ISRAEL = new CountryId(ISRAEL_CODE);
 
     private long value;
 
